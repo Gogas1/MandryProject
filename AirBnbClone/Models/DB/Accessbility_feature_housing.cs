ae08b1da1337d13679fed217c465630b91fe8c4d
Sagof1 <valerakargap@gmail.com>
2024-06-04 19:53:16 +0200

    models

--- AirBnbClone/Models/DB/Accessbility_feature_housing.cs
+++ AirBnbClone/Models/DB/Accessbility_feature_housing.cs
@@ -2,5 +2,9 @@
 {
     public class Accessbility_feature_housing
     {
+        public int id { get; set; }
+        public Accessbility_feature accessbility_feature_id { get; set; }
+        public Images image_id { get; set; }
+        public Housing housing_id { get; set; }
     }
 }
